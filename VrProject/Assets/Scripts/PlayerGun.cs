47fdf841b75ba1b29897ae6c9d9a30d46ce698e4
Harley Bekker <harleybekker@outlook.com>
2021-06-11 15:43:57 +1000

    added a holster to the player, gun now auto holsters itself when dropped

--- VrProject/Assets/Scripts/PlayerGun.cs
+++ VrProject/Assets/Scripts/PlayerGun.cs
@@ -3,30 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerGun : MonoBehaviour
 {
-    public float m_Damage = 20f;
+    public int m_Damage = 20;
     public int m_Ammo = 6;
 
     public float m_TimeBetweenShots = 0.3f;
     private float m_TimeUntilNextShot = 0f;
 
     public Transform m_Model;
+    public Transform m_HolsterPos;
     public Transform m_FirePoint;
 
-    [Space]
+    [HideInInspector]
     public bool m_IsHeld;
+    [HideInInspector]
     public OVRInput.Controller m_Controller;
+
+    private Rigidbody m_RB;
+
+    void Start()
+    {
+        m_RB = GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
-        if (m_IsHeld && m_Ammo > 0 && m_TimeUntilNextShot <= 0f && OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, m_Controller))
+
+        if (m_IsHeld)
         {
-            Shoot();
-            --m_Ammo;
-            m_TimeUntilNextShot = m_TimeBetweenShots;
+            /* Enable physics */
+            m_RB.isKinematic = false;
+
+            /* Fire the gun */
+            if (m_Ammo > 0 && m_TimeUntilNextShot <= 0f && OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, m_Controller))
+            {
+                Shoot();
+                --m_Ammo;
+                m_TimeUntilNextShot = m_TimeBetweenShots;
+            }
         }
-        else if (m_TimeUntilNextShot > 0f)
+        else
+        {
+            /* Position to the holster */
+            transform.position = Vector3.Lerp(transform.position, m_HolsterPos.position, 0.5f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, m_HolsterPos.rotation, 0.5f);
+
+            /* Disable physics */
+            m_RB.isKinematic = true;
+        }
+
+        if (m_TimeUntilNextShot > 0f)
         {
             m_TimeUntilNextShot -= Time.deltaTime;
         }
@@ -41,7 +69,7 @@ public class PlayerGun : MonoBehaviour
         {
             if (hit.transform.CompareTag("Enemy"))
             {
-                //hit.transform.GetComponent<Enemy>().TakeDamage(m_Damage);
+                hit.transform.GetComponent<Health>().DetachHealth(m_Damage);
             }
         }
     }
